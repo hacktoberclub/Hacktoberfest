1c414ebb473bf2e10087318dee3d127b0f8878ef
CharlesBFerguson <fergurburger326@gmail.com>
2022-10-13 13:53:03 -0400

    i added a working example for someone that needs help they just need to tweek it.

--- fixMe.js
+++ fixMe.js
@@ -3,7 +3,7 @@
 function check_array(arr1, arr2) = {
   // here are the arrays that i'm checking.
   const  arr1 = [];
-  const arr2 = [];\
+  const arr2 = [];
   const result = [];
 
   if(arr1[i] === arr2[j]){
@@ -13,3 +13,28 @@ function check_array(arr1, arr2) = {
 }
 
 // Happy hacking!
+
+
+const friend1 = ['james','mac','jess','arron','mike'];
+
+const friend2 = ['kate','tom','jeff','jake','arron','karen'];
+
+// if there are any mutual friends i will push them into here.
+const mutualFriends = []
+
+// to check ill use a for loop to to check in the index of the arrays match. im going to need to nest them in order for this to work.5r
+
+for(let i = 0; i < friend1.length; i++){
+    for(let j = 0; j < friend2.length; j++){
+        if(friend1[i] === friend2[j]){
+            mutualFriends.push(friend1[i]);
+            console.log('A change has been made in mutual friends');
+        } else{
+          console.log("An Error has happened, Please check your code and try again.");
+        }
+    }
+}
+
+
+// if it didn't work this should be empty..
+console.log(mutualFriends);
