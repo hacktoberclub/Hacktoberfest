c50a16330e77156771fe55c3bfe0bb11cad4521e
CharlesBFerguson <fergurburger326@gmail.com>
2022-10-13 12:16:09 -0400

    add a file for someone to fix

---
+++ fixMe.js
@@ -0,0 +1,15 @@
+// so for this program, I'm trying to create a function that will check an if my array matches anything in the other array please help me fix it! :)
+
+function check_array(arr1, arr2) = {
+  // here are the arrays that i'm checking.
+  const  arr1 = [];
+  const arr2 = [];\
+  const result = [];
+
+  if(arr1[i] === arr2[j]){
+    // find a way to "push" the matching item into the result array
+  }
+
+}
+
+// Happy hacking!
