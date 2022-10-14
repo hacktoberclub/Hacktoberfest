578dadc32ce3afaab91cc4dc522a96e7770d9308
CharlesBFerguson <fergurburger326@gmail.com>
2022-10-14 12:04:58 -0400

    added two new files

--- fixMe.js
+++ fixMe.js
@@ -24,35 +24,6 @@ for(let i = 0; i < friend1.length; i++){
 console.log(mutualFriends);
 // ===================================================================================================================================================================================================
 // Now you try 
-
-
-
-
-
-
-
-
-// so for this program, I'm trying to create a function that will check an if my array matches anything in the other array please help me fix it! :)
-function check_array(arr1, arr2){
-  // here are the arrays that i'm checking.
-  const  arr1 = [];
-  const arr2 = [];
-  const result = [];
-
-  if(arr1[a] === arr2[b]) {
-  
-    for(let a = 0; a < arr1.length; a++){
-      for(let b = 0; b < arr2.length; b++){
-        if(arr1[a] === arr2[b]{
-           // find a way to "push" the matching item into the result array
-          
-        })
-      }
-    }
-  }
-
-}
-
 // Happy hacking!
 
 
